0d0b62f91f62f2c12717c6a9a0c1988e5384df5d
bencelder <ben@hntvlan5310365.wlan.sas.upenn.edu>
2015-03-24 16:23:22 -0400

    move boxes

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 function initialize(){
-    alert("hello!");
+    //alert("hello!");
+    c = document.getElementById("myCanvas");
+    ctx = c.getContext("2d");
+
+    Xpos = 0;
+    Ypos = 0;
+
+}
+
+function drawBox(x, y){
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(x, y, 20, 20);
+}
+
+function keyDown(e){
+    //alert("Keycode: " + e.keyCode);
+    kc = e.keyCode;
+    if(kc == 37){
+        Xpos = Xpos - 1;
+    }
+    if(kc == 38){
+        Ypos = Ypos - 1;
+    }
+    if(kc == 39){
+        Xpos = Xpos + 1;
+    }
+    if(kc == 40){
+        Ypos = Ypos + 1;
+    }
+
+    drawBox(Xpos, Ypos);
 }
